535ac9a44b4497eac38660adcc69d7a1dd83c07d
AllEyezOnMe <bradleynortondodds@gmail.com>
2018-07-03 11:23:47 -0700

    Initial Commit

---
+++ BankingApp/Login.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Bradley Dodds on 7/3/18.
+//  Copyright © 2018 brad. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+
+
+}
+
